c2c9b0808066ca8f2c9c2c4e0d7b6258c19c2763
milesangelo <milesvendetti@gmail.com>
2021-02-25 18:30:37 -0800

    Adds a delete button component

--- ClientApp/src/components/buttons/DeleteButton.js
+++ ClientApp/src/components/buttons/DeleteButton.js
@@ -5,16 +5,12 @@ const DeleteButton = (props) => {
     const { isDeleting, name, onClick, index } = props;
     const btnText = name + " " + index
 
-
-    console.log('DeleteButton')
     return (
         <button 
             className="btn btn-sm btn-danger"
             name={name}
             onClick={() => { 
-                console.log('DeleteButton calling onClick() ', index);
                 onClick(index);
-                // console.log('DeleteButton calling onClick() ', index);
              }}
             disabled={isDeleting}
         >
@@ -24,22 +20,6 @@ const DeleteButton = (props) => {
             }
         </button>
     )
-
-    // return (
-    //     <div>
-    //         <button 
-    //             onClick={} 
-    //             index={grp.id}
-    //             className="btn btn-sm btn-danger btn-delete-group" 
-    //             name="addGroup"
-    //             disabled={isDeleting}>
-    //                 {grp.isDeleting
-    //                     ? <span className="spinner-border spinner-border-sm"></span>
-    //                     : <span>{buttonText}</span>
-    //                 }
-    //         </button>
-    //     </div>
-    // )
 }
 
 export default DeleteButton;
