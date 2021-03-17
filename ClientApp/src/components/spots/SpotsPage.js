d86e5d876c82741bdbbda71d48c541c6f647c715
milesangelo <milesvendetti@gmail.com>
2021-03-16 20:57:24 -0600

    Adds Spots page and tests

---
+++ ClientApp/src/components/spots/SpotsPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+const SpotsPage = (
+
+) => 
+
+    <div >
+        <Button 
+            variant="primary" 
+            size="lg" 
+            type="button" 
+            data-testid="add-button" 
+        >
+            Add
+        </Button>
+    </div>;
+
+export { SpotsPage };
