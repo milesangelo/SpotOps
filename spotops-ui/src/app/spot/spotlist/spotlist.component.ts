9bc249c49d9adb1e76526b0c4f03db48aa5b6f6e
milesangelo <milesvendetti@gmail.com>
2021-10-21 12:12:06 -0600

    Cleans up unused code in spotlist component

--- spotops-ui/src/app/spot/spotlist/spotlist.component.ts
+++ spotops-ui/src/app/spot/spotlist/spotlist.component.ts
@@ -1,20 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-spotlist',
-//   templateUrl: './spotlist.component.html',
-//   styleUrls: ['./spotlist.component.css']
-// })
-// export class SpotlistComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-// }
-
-
 import {Component} from '@angular/core';
 
 export interface Spot {
@@ -28,17 +11,12 @@ const SPOT_DATA: Spot[] = [
   {name: 'Love Park', type: 'Street', location:'Philadelphia, PA', dateCreated: '10/10/2020'},
   {name: 'Woodward', type: 'Park', location:'Boulder, CO', dateCreated: '12/14/2019'}
 ];
-
-
-
-/**
- * @title Basic use of `<table mat-table>`
- */
 @Component({
   selector: 'app-spotlist',
   styleUrls: ['spotlist.component.css'],
   templateUrl: 'spotlist.component.html',
 })
+
 export class SpotlistComponent {
   displayedColumns: string[] = ['name', 'dateCreated', 'type', 'location'];
   dataSource = SPOT_DATA;
